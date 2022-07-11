c92e54eff757b421e9392b9e62cf404401f0d20a
lienahtan <91964343+lienahtan@users.noreply.github.com>
2022-07-11 16:14:26 +0800

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -25,7 +25,7 @@ def predicting(perioddata, dateDict, component_selected, rootcause_selected,):
     for row in filteredDf.values.tolist():
         timeDict[row[8]][row[0].strftime("%#d/%#m/%Y")] += float(row[4])
     
-    preddf = pd.DataFrame(timeDict.values)
+    preddf = pd.DataFrame(timeDict.values.items())
     
     st.write(preddf)
     
