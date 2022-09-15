d7a6a467d7ae896c98fa1e3e9c5f222480cfb9dc
lienahtan <91964343+lienahtan@users.noreply.github.com>
2022-09-15 23:41:28 +0800

    Update Overall.py

--- Overall.py
+++ Overall.py
@@ -416,7 +416,7 @@ def overall(df, automation, startDate, endDate, lastMonthfirstday, lastMonthlast
                 module_selected = st.selectbox ("Which module do you want to analyze?", queryOptions)
                 measure_selected = st.selectbox ("Which measure do you want to analyze?", ['Total time', 'Count', 'Distribution'])
 #                 st.markdown('Components of selected module (Scroll)')
-#                 componentBreakdown = componentTable(perioddata, format, module_selected, prevdata)
+                componentBreakdown = componentTable(perioddata, format, module_selected, prevdata)
 #                 st.write(componentBreakdown[0], use_container_width=True)
                 
             with row1_col4:
