6073c3f5265fa70ab082270c961033a7eb41d55a
lienahtan <91964343+lienahtan@users.noreply.github.com>
2022-09-15 23:59:11 +0800

    Update Operator.py

--- Operator.py
+++ Operator.py
@@ -73,82 +73,82 @@ def operator(df, automation, startDate, endDate, lastMonthfirstday, lastMonthlas
     over_theme = {'txc_inactive': 'black','menu_background':'#ADD8E6','txc_active':'#0047AB','option_active':'white'}
     # over_theme = {'txc_inactive': '#FFFFFF'} 
     
-    with placeholder.container():
-        menu_id = hc.nav_bar(menu_definition=menu_data, home_name='Overview', override_theme=over_theme, sticky_mode='pinned')
+#     with placeholder.container():
+#         menu_id = hc.nav_bar(menu_definition=menu_data, home_name='Overview', override_theme=over_theme, sticky_mode='pinned')
         
-        if menu_id == "Overview":
+#         if menu_id == "Overview":
             
-            st.subheader("For period " + str(startDate.date()) + ' to ' + str(endDate.date()))
+#             st.subheader("For period " + str(startDate.date()) + ' to ' + str(endDate.date()))
             
-            main1, main2, main3, main4   = st.columns((0.5,0.5,1,1))
+#             main1, main2, main3, main4   = st.columns((0.5,0.5,1,1))
             
-            with main1:
-                operatorNames = ''
-                for i in operators:
-                    operatorNames += i
-                    operatorNames += '\n\n'
-                st.markdown(f'<h2 style="text-align: left; color:#2F847C; font-size:18px;">{"Operators present:"}</h1>', unsafe_allow_html=True)
-                st.success(operatorNames)
+#             with main1:
+#                 operatorNames = ''
+#                 for i in operators:
+#                     operatorNames += i
+#                     operatorNames += '\n\n'
+#                 st.markdown(f'<h2 style="text-align: left; color:#2F847C; font-size:18px;">{"Operators present:"}</h1>', unsafe_allow_html=True)
+#                 st.success(operatorNames)
             
-            with main2:
-                st.markdown(f'<h2 style="text-align: left; color:black; font-size:18px;">{"Best Operator: "}</h1>', unsafe_allow_html=True)
-                st.info('Alvin')
-                st.markdown(f'<h2 style="text-align: left; color:black; font-size:18px;">{"Most Improved Operator: "}</h1>', unsafe_allow_html=True)
-                st.info('Nathaniel')
+#             with main2:
+#                 st.markdown(f'<h2 style="text-align: left; color:black; font-size:18px;">{"Best Operator: "}</h1>', unsafe_allow_html=True)
+#                 st.info('Alvin')
+#                 st.markdown(f'<h2 style="text-align: left; color:black; font-size:18px;">{"Most Improved Operator: "}</h1>', unsafe_allow_html=True)
+#                 st.info('Nathaniel')
                 
-            with main3:
-                hc.info_card(title='Average Operators EXP', content='All good!', sentiment='good', bar_value=77, icon_size="2.4rem",title_text_size="1.8rem",content_text_size="1.4rem")
-                with hc.HyLoader('Loading',hc.Loaders.pulse_bars,):
-                    time.sleep(0.5)
+#             with main3:
+#                 hc.info_card(title='Average Operators EXP', content='All good!', sentiment='good', bar_value=77, icon_size="2.4rem",title_text_size="1.8rem",content_text_size="1.4rem")
+#                 with hc.HyLoader('Loading',hc.Loaders.pulse_bars,):
+#                     time.sleep(0.5)
                     
-            with main4:
-                hc.info_card(title='Empty Field', content='Empty Field', sentiment='good', bar_value=77, icon_size="2.4rem",title_text_size="1.8rem",content_text_size="1.4rem")
-                with hc.HyLoader('Loading',hc.Loaders.pulse_bars,):
-                    time.sleep(0.5)
+#             with main4:
+#                 hc.info_card(title='Empty Field', content='Empty Field', sentiment='good', bar_value=77, icon_size="2.4rem",title_text_size="1.8rem",content_text_size="1.4rem")
+#                 with hc.HyLoader('Loading',hc.Loaders.pulse_bars,):
+#                     time.sleep(0.5)
                     
                     
-            info1, info2, info3   = st.columns((0.5,1,1))
+#             info1, info2, info3   = st.columns((0.5,1,1))
             
-            with info1:
-                st.subheader("Per Alarm Type")
+#             with info1:
+#                 st.subheader("Per Alarm Type")
                 
-            with info2:
-                st.subheader("Most Recovery Done 1EXP")
+#             with info2:
+#                 st.subheader("Most Recovery Done 1EXP")
                 
-            with info3:
-                st.subheader("Fastest Reovery Time 1EXP")
+#             with info3:
+#                 st.subheader("Fastest Reovery Time 1EXP")
                 
                 
                 
-            info11, info22, info33   = st.columns((0.5,1,1))
+#             info11, info22, info33   = st.columns((0.5,1,1))
             
-            with info1:
-                st.subheader("Cumulative")
+#             with info1:
+#                 st.subheader("Cumulative")
                 
-            with info2:
-                st.subheader("Most experience based on count 1EXP")
+#             with info2:
+#                 st.subheader("Most experience based on count 1EXP")
                 
-            with info3:
-                st.subheader("Most Experience based on time 1EXP")
+#             with info3:
+#                 st.subheader("Most Experience based on time 1EXP")
             
             
             
             
             
             
-        if menu_id == "Individual Operators":
+#         if menu_id == "Individual Operators":
             
-            operator1, operator2 = st.columns((1,1))
+#             operator1, operator2 = st.columns((1,1))
             
-            with operator1:
-                st.header('Operator')
+#             with operator1:
+#                 st.header('Operator')
 
-            with operator2:
-                st.header('EXP Chart')
+#             with operator2:
+#                 st.header('EXP Chart')
     
         
-        if menu_id == "View Dataset":
-            st.write(perioddata)
+#         if menu_id == "View Dataset":
+#             st.write(perioddata)
         
         
         
